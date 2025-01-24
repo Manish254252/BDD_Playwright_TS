39d241446e67f92cbc97156090b6ee7d1a4bacce
Manish Solanki <70832573+Manish254252@users.noreply.github.com>
2025-01-24 17:47:43 +0530

    added code in login steps

--- src/test/steps/login_step.ts
+++ src/test/steps/login_step.ts
@@ -1,22 +1,36 @@
+import {Given, When, Then, setDefaultTimeout} from "@cucumber/cucumber";
 
-
-import {Given, When, setDefaultTimeout} from "@cucumber/cucumber";
-
-import { expect} from "@playwright/test";
+import {expect} from "@playwright/test";
 import {pageFixture} from "../../hooks/pageFixtures";
 import {page} from "../../hooks/hooks";
+import {LoginPage} from "../pages/loginPage";
 
 // import {page} from "../../hooks/hooks";
+// import {LoginPage} from "../pages/loginPage";
 
-setDefaultTimeout(60 * 1000);
 
+// setDefaultTimeout(60 * 1000);
 
+let loginPage:LoginPage ;
+console.log(pageFixture.page+" in steps ")
 Given('user visits the web page', async function () {
-    await pageFixture.page.goto('https://playwright.dev/');
+        loginPage  = new LoginPage(pageFixture.page);
+    await loginPage.navigateToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+});
+When('user enter username',async function () {
+
+   await loginPage.enterUsername("Admin")
 
 });
 
 
-When('user click link', async function () {
-    await expect(pageFixture.page).toHaveTitle(/Playwright/);
+When('user enter password',async function () {
+  await  loginPage.enterPassword("admin123")
 });
+
+
+Then('user clicks on signIn Button', async function () {
+
+  await  loginPage.clickLoginButton()
+});
+
