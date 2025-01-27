a40a1e55485fdba2785c509c52f0af738ae794fa
Manish Solanki <70832573+Manish254252@users.noreply.github.com>
2025-01-27 14:08:59 +0530

    added code admin page

--- src/test/pages/AdminPage.ts
+++ src/test/pages/AdminPage.ts
@@ -1,16 +1,24 @@
 import {Page} from "playwright";
 import {Locator} from "@playwright/test";
+import {expect} from "@playwright/test";
 
 export class AdminPage{
     addBtn: Locator;
+    userNameAdminList: Locator;
 
     constructor(page:Page) {
         // page = pageFixture.page;
         console.log(page);
         this.addBtn = page.locator("//button[normalize-space()='Add']");
+        this.userNameAdminList = page.locator("//div[@role='table']//div[@role='row']/div[2]");
     }
 
     async clickAddBtn(){
         await this.addBtn.click();
+    }
+    async verifyUserInAdminList(userName: string){
+        let userNames = await this.userNameAdminList.allInnerTexts();
+        console.log(userNames);
+        expect(userNames).toContain(userName);
     }
 }
